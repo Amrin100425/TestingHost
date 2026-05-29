83ecf0f32b5889b0f411556d6359623abb395c13
amrin2509 <sorcheatamrin954@gmail.com>
2026-05-29 15:31:06 +0700

    new

--- api/login.js
+++ api/login.js
@@ -6,7 +6,9 @@ module.exports = async (req, res) => {
   if (req.method !== "POST")
     return res.status(405).json({ error: "Method not allowed" });
 
-  const { username, password } = req.body;
+  const { username, password } =
+    typeof req.body === "string" ? JSON.parse(req.body) : req.body || {};
+
   if (!username || !password)
     return res.status(400).json({ error: "Username and password required" });
 
